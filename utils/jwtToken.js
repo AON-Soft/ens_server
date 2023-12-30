96c30812dd4ff03f6033736d0802f3ddfc07ca66
Alamgir Ahamed <alamgir72435@gmail.com>
2023-12-30 16:44:10 +0600

    udpates

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,24 +1,19 @@
 const { COOKIE_EXPIRE } = require("../constant");
 
 const sendToken = (user, statusCode, res) => {
+  console.log("============user====================", user);
+  const token = user.getJWTToken();
+  const cookieExpire = COOKIE_EXPIRE;
 
-  try {
-    console.log("============user====================", user);
-    const token = user.getJWTToken();
-    const cookieExpire = COOKIE_EXPIRE;
-  
-    const options = {
-      expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-    };
-  
-    res
-      .status(statusCode)
-      .cookie("token", token, options)
-      .json({ success: true, user, token });
-  } catch (error) {
-    console.log("============error====================", error);
-  }
+  const options = {
+    expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, user, token });
 };
 
 module.exports = sendToken;
