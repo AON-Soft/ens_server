5cdbe1d5c849b743c025ee19b4b67457fdb18b7c
Alamgir Ahamed <alamgir72435@gmail.com>
2023-12-30 15:51:44 +0600

    testing bug

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,19 +1,24 @@
+const { COOKIE_EXPIRE } = require("../constant");
+
 const sendToken = (user, statusCode, res) => {
-  console.log("============user====================", user);
-  const token = user.getJWTToken();
-  const cookieExpire = process.env.COOKIE_EXPIRE
-    ? process.env.COOKIE_EXPIRE
-    : 2;
 
-  const options = {
-    expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-  };
-
-  res
-    .status(statusCode)
-    .cookie("token", token, options)
-    .json({ success: true, user, token });
+  try {
+    console.log("============user====================", user);
+    const token = user.getJWTToken();
+    const cookieExpire = COOKIE_EXPIRE;
+  
+    const options = {
+      expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+  
+    res
+      .status(statusCode)
+      .cookie("token", token, options)
+      .json({ success: true, user, token });
+  } catch (error) {
+    console.log("============error====================", error);
+  }
 };
 
 module.exports = sendToken;
