c01120af732d07ad3d662967555937311110329f
Md Ariful Islam <arifislam11ctg@gmail.com>
2024-01-06 16:31:05 +0600

    Update Shop Issue Fixed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,91 +1,82 @@
-const ErrorHander = require("../utils/errorhander");
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const Shop = require("../models/shopModel");
-const catchAsyncError = require("./catchAsyncError");
-const { JWT_SECRET } = require("../constant");
-
-
-
-
-
-
+const ErrorHander = require('../utils/errorhander')
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
+const Shop = require('../models/shopModel')
+const catchAsyncError = require('./catchAsyncError')
+const { JWT_SECRET } = require('../constant')
 
 // Universal Authentication Middleware
 exports.isAuthenticated = catchAsyncError(async (req, _, next) => {
   //   const { token } = req.cookies;
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers['authorization']
 
-  if (typeof authHeader === "undefined") {
-    return next(new ErrorHander("un-authorized", 401));
+  if (typeof authHeader === 'undefined') {
+    return next(new ErrorHander('un-authorized', 401))
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1]
   if (!token) {
-    return next(new ErrorHander("Please Login to access this resource", 401));
+    return next(new ErrorHander('Please Login to access this resource', 401))
   }
-  const secret = JWT_SECRET;
+  const secret = JWT_SECRET
 
-  const decodedData = jwt.verify(token, secret);
-  req.user = decodedData;
-  next();
-});
-
-
-
-
+  const decodedData = jwt.verify(token, secret)
+  req.user = decodedData
+  console.log(req.user)
+  next()
+})
 
 exports.isAuthenticatedShop = catchAsyncError(async (req, _, next) => {
-  const getShop = await Shop.findOne({ createdBy: req.user._id });
+  const getShop = await Shop.findOne({ createdBy: req.user.id })
   if (!getShop) {
-    return next(new ErrorHander("Shop not found", 404));
+    return next(new ErrorHander('Shop not found', 404))
   }
-  req.shop = getShop;
-  next();
-});
+  req.shop = getShop
+  next()
+})
 
 exports.isAuthenticatedUserTemp = catchAsyncError(async (req, _, next) => {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers['authorization']
 
-  if (typeof authHeader === "undefined") {
-    return next(new ErrorHander("Authorization Header is Undefined", 401));
+  if (typeof authHeader === 'undefined') {
+    return next(new ErrorHander('Authorization Header is Undefined', 401))
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1]
 
   if (!token) {
     return next(
       new ErrorHander(
-        "Please register first Or Your Cookies is not working",
-        401
-      )
-    );
+        'Please register first Or Your Cookies is not working',
+        401,
+      ),
+    )
   }
-  const secret = JWT_SECRET;
-  const decodedData = jwt.verify(token, secret);
-  req.user = decodedData;
-  next();
-});
+  const secret = JWT_SECRET
+  const decodedData = jwt.verify(token, secret)
+  req.user = decodedData
+  next()
+})
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, _, next) => {
   //   const { token } = req.cookies;
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers['authorization']
 
-  if (typeof authHeader === "undefined") {
-    return next(new ErrorHander("Authorization Header is Undefined", 401));
+  if (typeof authHeader === 'undefined') {
+    return next(new ErrorHander('Authorization Header is Undefined', 401))
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1]
   if (!token) {
-    return next(new ErrorHander("Please Login to access this resource", 401));
+    return next(new ErrorHander('Please Login to access this resource', 401))
   }
-  const secret = process.env.JWT_SECRET || "fjhhIOHfjkflsjagju0fujljldfgl";
-  const decodedData = jwt.verify(token, secret);
+  const secret = process.env.JWT_SECRET || 'fjhhIOHfjkflsjagju0fujljldfgl'
+  const decodedData = jwt.verify(token, secret)
 
-  req.user = await User.findById(decodedData.id);
+  req.user = await User.findById(decodedData.id)
 
-  next();
-});
+  next()
+})
 
 exports.isAuthorizeRoles = (...roles) => {
   return (req, _, next) => {
@@ -94,12 +85,12 @@ exports.isAuthorizeRoles = (...roles) => {
         return next(
           new ErrorHander(
             `Role: ${req.user.role} is not allowed to access this resource`,
-            403
-          )
-        );
+            403,
+          ),
+        )
       }
-    });
+    })
 
-    next();
-  };
-};
+    next()
+  }
+}
