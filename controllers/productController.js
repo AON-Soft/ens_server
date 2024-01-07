ae6edb660ef5efd6ed3597e67a6ef2b21d66b766
Alamgir Ahamed <alamgir72435@gmail.com>
2024-01-07 10:07:15 +0600

    updates

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,10 @@
 const Product = require('../models/productModel')
-const Categories = require('../models/categoryModel')
 const Shop = require("../models/shopModel");
 const ErrorHandler = require('../utils/errorhander')
 const catchAsyncError = require('../middleware/catchAsyncError')
 const ApiFeatures = require('../utils/apifeature')
 
-exports.createProduct = catchAsyncError(async (req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res) => {
   req.body.user = req.user.id
   
   const shop = await Shop.findOne({ userId: req.user.id });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,6 @@ const express = require('express')
 const {
   isAuthenticated,
   isAuthorizeRoles,
-  isAuthenticatedShop,
 } = require('../middleware/auth')
 
 const {
