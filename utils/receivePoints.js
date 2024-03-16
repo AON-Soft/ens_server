010134977d3754629f84f1c481435c3ce20762dc
Alamgir Ahamed <alamgir72435@gmail.com>
2024-03-16 10:05:22 +0600

    Update health checker response

--- utils/receivePoints.js
+++ utils/receivePoints.js
@@ -1,19 +1,19 @@
 const User = require("../models/userModel");
-const Balance = require("../models/mainBalanceModel");
+// const Balance = require("../models/mainBalanceModel");
 
-const receivePoints = async (email, points, role) => {
+const receivePoints = async (email, _, role) => {
   const receiver = await User.findOne({ email: email, role: role });
 
   if (!receiver) {
     // return ErrorHandler(`${role} not found`, 404);
   }
 
-  const mainBalance = await Balance.findOne({ user: receiver._id });
-  if (!mainBalance) {
-    // return ErrorHandler(`Balance not found for this ${role}`, 404);
-  }
-  mainBalance.Balance += points;
-  await mainBalance.save();
+  // const mainBalance = await Balance.findOne({ user: receiver._id });
+  // if (!mainBalance) {
+  //   // return ErrorHandler(`Balance not found for this ${role}`, 404);
+  // }
+  // mainBalance.Balance += points;
+  // await mainBalance.save();
   return receiver;
 };
 
